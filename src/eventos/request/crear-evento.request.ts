a814dbb9954c7451c72f787d00ecb4f9ddebe6a9
Alejandro Giorgi <eze.giorgi@gmail.com>
2024-08-03 18:09:01 -0300

    Mapeo del request de creacion de eventos

--- src/eventos/request/crear-evento.request.ts
+++ src/eventos/request/crear-evento.request.ts
@@ -1,8 +1,33 @@
-import { Expose } from 'class-transformer';
-import { IsNotEmpty } from 'class-validator';
+import { Expose, Type } from 'class-transformer';
+import { ArrayMinSize, IsEmail, IsNotEmpty, IsNumber, IsPositive, ValidateNested } from 'class-validator';
 
-export class CrearRequest {
-    @Expose({name: 'nombre_evento'})
+export class Participante {
     @IsNotEmpty()
     nombre: string
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string
+}
+
+export class CrearRequest {
+
+    @Expose({name: 'nombre_evento'})
+    @IsNotEmpty({message: 'El nombre del evento es requerido'})
+    nombre: string;
+
+    @IsNotEmpty()
+    @ValidateNested()
+    @Type(() => Participante)
+    organizador: Participante;
+
+    @ArrayMinSize(1, {message: 'El evento debe contener al menos un participante'})
+    @ValidateNested({each: true})
+    @Type(() => Participante)
+    participantes: Array<Participante>;
+
+    @Expose({name: 'monto_maximo'})
+    @IsNumber({maxDecimalPlaces: 2}, {message: 'El monto máximo debe ser un número con dos decimales'})
+    @IsPositive({message: 'El monto máximo debe ser un número mayor a cero'})
+    montoMaximo: number;
 }
--- test/eventos.e2e-spec.ts
+++ test/eventos.e2e-spec.ts
@@ -19,7 +19,18 @@ describe('Eventos (e2e)', () => {
 
   it('Crear evento válido', () => {
     const requestValido = {
-      nombre_evento: 'Navidad 2024'
+      nombre_evento: 'Navidad 2024',
+      organizador: {
+        nombre: 'Alejandro',
+        email: 'ale@gmail.com'
+      },
+      participantes: [
+        {
+          nombre: 'Dani',
+          email: 'dani@gmail.com'
+        }
+      ],
+      monto_maximo: 25000.00
     }
     return request(app.getHttpServer())
       .post('/eventos')
@@ -38,5 +49,54 @@ describe('Eventos (e2e)', () => {
         expect(response.body.message).toBeDefined();
         expect(response.body.message[0]).toContain("nombre");
       })
+  });
+
+  it('No se puede crear eventos sin nombre de organizador', () => {
+    return request(app.getHttpServer())
+    .post('/eventos')
+    .send({organizador: {}})
+    .expect(400)
+    .then(response => {
+      expect(response.body.message[1]).toContain("nombre");
+    })
+  });
+
+  it('No se puede crear eventos sin email de organizador', () => {
+    return request(app.getHttpServer())
+    .post('/eventos')
+    .send({organizador: {}})
+    .expect(400)
+    .then(response => {
+      expect(response.body.message[3]).toContain("email");
+    })
+  });
+
+  it('Un evento debe tener al menos un participante', () => {
+    return request(app.getHttpServer())
+    .post('/eventos')
+    .send({participantes: []})
+    .expect(400)
+    .then(response => {
+      expect(response.body.message[2]).toContain("al menos un participante");
+    })
+  });
+
+  it('Los participantes deben tener nombre', () => {
+    return request(app.getHttpServer())
+    .post('/eventos')
+    .send({participantes: [{}]})
+    .expect(400)
+    .then(response => {
+      expect(response.body.message[2]).toContain("participantes.0.nombre");
+    })
+  });
+
+  it('No se puede crear un evento sin un monto máximo establecido', () => {
+    return request(app.getHttpServer())
+    .post('/eventos')
+    .expect(400)
+    .then(response => {
+      expect(response.body.message[3]).toContain("monto máximo");
+    })
   });
 });
